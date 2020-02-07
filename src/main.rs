02bea42cd002cf911aac0f67a6974e3dcc072109
sean <sean.jennings1@gmail.com>
2020-02-07 05:53:20 +0000

    Set socket address to beaglebone.local

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 use warp::Filter;
+use std::net::ToSocketAddrs;
 
 #[tokio::main]
 async fn main() {
     let cors = warp::cors()
-        .allow_origin("http://localhost:8000")
+        .allow_origin("http://beaglebone.local")
         .allow_methods(vec!["GET", "POST"]);
 
     // GET /hello/warp => 200 OK with body "Hello, warp!"
     let hello = warp::path!("hello" / String)
         .map(|name| format!("Hello, {}!", name))
         .with(cors);
+    let sock_addr = "beaglebone.local:3030".to_socket_addrs()
+        .unwrap()
+        .next().unwrap();
     warp::serve(hello)
-        .run(([127, 0, 0, 1], 3030))
+        .run(sock_addr)
         .await;
 }
